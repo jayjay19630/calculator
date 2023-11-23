712c845213a6a0f0230475c4756c85a564e2ef60
positivelyjon <positivelyjon@gmail.com>
2023-11-23 21:33:22 +0800

    Create calculator display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let first_operand = undefined;
+let second_operand = undefined;
+let operator = undefined;
+
 function apply_operator(first_operand, second_operand, operator) {
     if (operator === "+") {
         return first_operand + second_operand;
